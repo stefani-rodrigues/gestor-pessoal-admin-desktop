caac99a17042c8bfc2df68b76037ccff268290e2
Stefani Rodrigues De Oliveira <stefani.deoliveira19@gmail.com>
2025-11-24 12:14:33 -0300

    Correçoes de bugs e implementacao completa Usuario.

--- src/main/java/com/senac/gestorpessoaladmimconfig/model/DAO/EnderecoDAO.java
+++ src/main/java/com/senac/gestorpessoaladmimconfig/model/DAO/EnderecoDAO.java
@@ -1,4 +1,20 @@
 package com.senac.gestorpessoaladmimconfig.model.DAO;
 
+import com.senac.gestorpessoaladmimconfig.model.Endereco;
+import jakarta.persistence.EntityManager;
+
 public class EnderecoDAO {
+   private EntityManager entityManager;
+
+   public EnderecoDAO (EntityManager entityManager){
+       this.entityManager = entityManager;
+   }
+
+   public void salvar(Endereco e){
+       entityManager.getTransaction().begin();
+
+       entityManager.persist(e);
+
+       entityManager.getTransaction().commit();
+   }
 }
--- src/main/java/com/senac/gestorpessoaladmimconfig/model/Endereco.java
+++ src/main/java/com/senac/gestorpessoaladmimconfig/model/Endereco.java
@@ -1,4 +1,42 @@
 package com.senac.gestorpessoaladmimconfig.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String cep;
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(length = 500)
+    private String endereco;
 }
